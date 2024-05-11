41684ca821e2be3ed72524caac3e3b9db1c1fb7e
Dolevkle <dolevkle@gmail.com>
2024-05-11 03:44:23 +0300

    [ADDED] - basic home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { CreatePost } from "~/app/_components/create-post";
 import { Button } from "~/components/ui/button";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
+import GlobeShowcase from "./_components/GlobeShowcase";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -15,10 +16,19 @@ export default async function Home() {
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           <span className="text-white">Heartbeat</span>
         </h1>
-        <div className="flex justify-between w-52">
+        <h2 className="text-center text-xl font-bold text-black dark:text-white md:text-4xl">
+          Connecting
+          &nbsp;<span className="text-primary">Hearts</span>&nbsp;
+           with music worldwide
+        </h2>
+        {/* <h3 className="mx-auto mt-2 max-w-md text-center text-base font-normal text-neutral-700 dark:text-neutral-200 md:text-lg">
+          The leading AI personality matcher in the world.
+        </h3> */}
+        {/* <div className="flex w-52 justify-between">
           <Button variant="secondary">Sign Up</Button>
           <Button>Log In</Button>
-        </div>
+        </div> */}
+        <GlobeShowcase />
         {/* <CrudShowcase /> */}
       </div>
     </main>
