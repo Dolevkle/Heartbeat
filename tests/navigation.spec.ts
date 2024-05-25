1f061b0e841a68bc24c833b23d5c57cc4832e01d
Dolevkle <dolevkle@gmail.com>
2024-05-25 13:23:17 +0300

    [FIXED] - tests

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -4,7 +4,9 @@ test('should navigate to the login page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/')
   // Find an element with the text 'About' and click on it
-  await page.click('text=Log in')
+  const loginButton = page.locator('text=Log in');
+  await loginButton.waitFor({ state: 'visible' });
+  await loginButton.click()
 
   await page.waitForURL('**/login');
   // The new URL should be "/about" (baseURL is used there)
@@ -17,7 +19,9 @@ test('should navigate to the sign up page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/')
   // Find an element with the text 'About' and click on it
-  await page.click('text=Sign Up')
+  const signUpButton = page.locator('text=Sign Up');
+  await signUpButton.waitFor({ state: 'visible' });
+  await signUpButton.click()
 
   await page.waitForURL('**/signup');
   // The new URL should be "/about" (baseURL is used there)
