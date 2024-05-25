9940e3f931abf555609c4f5f8b82536c33b58490
Dolevkle <dolevkle@gmail.com>
2024-05-25 13:15:35 +0300

    [FIXED] - web server command might be wong

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     },
     webServer: {
         command: 'pnpm start',
-        url: 'http://127.0.0.1:3000',
+        port: 3000,
         timeout: 120 * 1000,
         reuseExistingServer: !process.env.CI,
         env: env
--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -5,6 +5,8 @@ test('should navigate to the login page', async ({ page }) => {
   await page.goto('/')
   // Find an element with the text 'About' and click on it
   await page.click('text=Log in')
+
+  await page.waitForURL('**/login');
   // The new URL should be "/about" (baseURL is used there)
   await expect(page).toHaveURL('/login')
   // The new page should contain an h1 with "About"
@@ -16,6 +18,8 @@ test('should navigate to the sign up page', async ({ page }) => {
   await page.goto('/')
   // Find an element with the text 'About' and click on it
   await page.click('text=Sign Up')
+
+  await page.waitForURL('**/signup');
   // The new URL should be "/about" (baseURL is used there)
   await expect(page).toHaveURL('/signup')
   // The new page should contain an h1 with "About"
