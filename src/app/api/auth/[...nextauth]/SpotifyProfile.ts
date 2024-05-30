c558b4711b19a2bcb67b67f8cb6d4efdd92ab7ff
Dolevkle <dolevkle@gmail.com>
2024-05-30 19:45:53 +0300

    [REFACTORED] - SpotifyProfile.ts put refresh token logic in comment for now

--- src/app/api/auth/[...nextauth]/SpotifyProfile.ts
+++ src/app/api/auth/[...nextauth]/SpotifyProfile.ts
@@ -1,15 +1,10 @@
-import { JWT } from "next-auth/jwt";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { env } from "~/env";
-import axios from "axios";
 
-if (!env.SPOTIFY_CLIENT_ID) {
-  throw new Error("Missing SPOTIFY_CLIENT_ID");
-}
+if (!env.SPOTIFY_CLIENT_ID) throw new Error("Missing SPOTIFY_CLIENT_ID");
 
-if (!env.SPOTIFY_CLIENT_SECRET) {
+if (!env.SPOTIFY_CLIENT_SECRET)
   throw new Error("Missing SPOTIFY_CLIENT_SECRET");
-}
 
 const spotifyProfile = SpotifyProvider({
   clientId: env.SPOTIFY_CLIENT_ID,
@@ -36,33 +31,33 @@ spotifyProfile.authorization = authURL.toString();
 
 export default spotifyProfile;
 
-export async function refreshAccessToken(token: JWT) {
-  try {
-    const refreshedTokens = await axios.post(
-      "https://accounts.spotify.com/authorize",
-      {
-        scope: scopes.join(" "),
-      },
-    );
-
-    if (!refreshedTokens) {
-      throw refreshedTokens;
-    }
-
-    return {
-      ...token,
-      access_token: refreshedTokens.access_token,
-      token_type: refreshedTokens.token_type,
-      expires_at: refreshedTokens.expires_at,
-      expires_in: (refreshedTokens.expires_at ?? 0) - Date.now() / 1000,
-      refresh_token: refreshedTokens.refresh_token ?? token.refresh_token,
-      scope: refreshedTokens.scope,
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    };
-  }
-}
+// export async function refreshAccessToken(token: JWT) {
+//   try {
+//     const refreshedTokens = await axios.post(
+//       "https://accounts.spotify.com/authorize",
+//       {
+//         scope: scopes.join(" "),
+//       },
+//     );
+//
+//     if (!refreshedTokens) {
+//       throw refreshedTokens;
+//     }
+//
+//     return {
+//       ...token,
+//       access_token: refreshedTokens.access_token,
+//       token_type: refreshedTokens.token_type,
+//       expires_at: refreshedTokens.expires_at,
+//       expires_in: (refreshedTokens.expires_at ?? 0) - Date.now() / 1000,
+//       refresh_token: refreshedTokens.refresh_token ?? token.refresh_token,
+//       scope: refreshedTokens.scope,
+//     };
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       ...token,
+//       error: "RefreshAccessTokenError",
+//     };
+//   }
+// }
