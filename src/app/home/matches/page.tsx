34c901fc1d8c157c9244657dfecc619a8bc62ebc
Dolevkle <dolevkle@gmail.com>
2024-06-02 01:44:06 +0300

    [REFACTORED] - removed redundant suspense

--- src/app/home/matches/page.tsx
+++ src/app/home/matches/page.tsx
@@ -1,15 +1,13 @@
 import Matches from "../../_components/matches";
 import { api } from "~/trpc/server";
 import { getServerAuthSession } from "~/server/auth";
-import { Suspense } from "react";
+
 export default async function Page() {
   const session = await getServerAuthSession();
   const matches = await api.user.getMatches(session?.user.id ?? "");
   return (
     <div className="h-full w-fit">
-      <Suspense fallback="Loading matches...">
-        <Matches matches={matches} />
-      </Suspense>
+      <Matches matches={matches} />
     </div>
   );
 }
