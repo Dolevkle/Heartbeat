19c13fe168189aeee878580963c9e1c36549c803
Dolevkle <dolevkle@gmail.com>
2024-06-02 01:40:55 +0300

    [REFACTORED] - added suspense to matches

---
+++ src/app/home/matches/loading.tsx
@@ -0,0 +1,12 @@
+export default function loading() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <div className="absolute h-32 w-32 animate-spin rounded-full border-b-4 border-t-4 border-primary/20"></div>
+      <img
+        src="/loading-image.png"
+        alt="no loading image"
+        className="h-28 w-28 rounded-full"
+      />
+    </div>
+  );
+}
