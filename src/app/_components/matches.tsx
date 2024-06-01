1e55fb15897645daca15842d8abaf77784d0d75c
Dolevkle <dolevkle@gmail.com>
2024-06-01 13:23:32 +0300

    [REFACTORED] - matches displays looks normal now without bugs

--- src/app/_components/matches.tsx
+++ src/app/_components/matches.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@components/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "@components/card";
 import { Sparkles } from "lucide-react";
 import { api, type RouterOutputs } from "~/trpc/react";
 import { useSession } from "next-auth/react";
+import { Skeleton } from "@components/skeleton";
 interface Props {
   matches: RouterOutputs["user"]["getMatches"];
 }
@@ -15,33 +17,45 @@ export default function Component({ matches }: Props) {
   const { data: users, isLoading } = api.user.getMatchUsers.useQuery(ids, {
     enabled: ids !== undefined,
   });
-  if (isLoading) return "kaki";
+
   return (
-    <Card className="h-full w-fit rounded-l-none rounded-r rounded-bl-none border-card">
+    <Card className="h-full w-64 rounded-l-none rounded-r rounded-bl-none border-card">
       <CardHeader>
         <CardTitle>Matches</CardTitle>
       </CardHeader>
       <CardContent className="grid gap-8">
-        {matches?.map((match, i) => (
-          <div key={match.id} className="flex items-center gap-4">
-            <Avatar className="hidden h-9 w-9 sm:flex">
-              <AvatarImage src={users[i]?.image ?? ""} alt="Avatar" />
-              <AvatarFallback>OM</AvatarFallback>
-            </Avatar>
-            <div className="grid gap-1">
-              <p className="text-sm font-medium leading-none">
-                {users[i]?.name}
-              </p>
-              <p className="text-sm text-muted-foreground">{users[i]?.age}</p>
+        {matches?.map((match, i) =>
+          isLoading ? (
+            <div key={match.id} className="flex items-center space-x-4">
+              <Skeleton className="h-12 w-12 rounded-full" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[75px]" />
+              </div>
             </div>
-            <div className="ml-auto flex items-center space-x-3 font-medium">
-              {parseFloat(match.similarity) >= HIGH_MATCH && (
-                <Sparkles className="h-4 w-4 stroke-primary" />
-              )}
-              <span>{match.similarity}</span>
+          ) : (
+            <div key={match.id} className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src={users[i]?.image ?? ""} alt="Avatar" />
+                <AvatarFallback>
+                  {users[i]?.name?.charAt(0) ?? ""}
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">
+                  {users[i]?.name}
+                </p>
+                <p className="text-sm text-muted-foreground">{users[i]?.age}</p>
+              </div>
+              <div className="ml-auto flex items-center space-x-3 font-medium">
+                {parseFloat(match.similarity) >= HIGH_MATCH && (
+                  <Sparkles className="h-4 w-4 stroke-primary" />
+                )}
+                <span>{match.similarity}</span>
+              </div>
             </div>
-          </div>
-        ))}
+          ),
+        )}
       </CardContent>
     </Card>
   );
--- src/app/home/matches/page.tsx
+++ src/app/home/matches/page.tsx
@@ -1,13 +1,9 @@
-"use client";
 import Matches from "../../_components/matches";
-import { useSession } from "next-auth/react";
-import { api } from "~/trpc/react";
-export default function Page() {
-  const session = useSession();
-  const { data: matches } = api.user.getMatches.useQuery(
-    session.data?.user.id ?? "",
-  );
-  console.log(matches);
+import { api } from "~/trpc/server";
+import { getServerAuthSession } from "~/server/auth";
+export default async function Page() {
+  const session = await getServerAuthSession();
+  const matches = await api.user.getMatches(session?.user.id ?? "");
   return (
     <div className="h-full w-fit">
       <Matches matches={matches} />
