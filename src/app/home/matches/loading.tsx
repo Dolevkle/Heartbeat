5cc50f74b7fef2f9c7a6af1e7f0eecdee6d03f86
Dolevkle <dolevkle@gmail.com>
2024-06-02 02:14:43 +0300

    [ADDED] - css change

--- src/app/home/matches/loading.tsx
+++ src/app/home/matches/loading.tsx
@@ -1,7 +1,7 @@
 export default function loading() {
   return (
     <div className="flex h-full items-center justify-center">
-      <div className="absolute h-32 w-32 animate-spin rounded-full border-b-4 border-t-4 border-primary/20"></div>
+      <div className="absolute h-32 w-32 animate-spin rounded-full border-b-4 border-t-4 border-primary"></div>
       <img
         src="/loading-image.png"
         alt="no loading image"
