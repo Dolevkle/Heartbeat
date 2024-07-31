0a4f9c877629d5a7ebb55ea18f766e8f24c87c26
טל דבי <taldebi2@gmail.com>
2024-07-31 23:53:07 +0300

    added match window in progress

--- src/app/home/matches/page.tsx
+++ src/app/home/matches/page.tsx
@@ -1,13 +1,15 @@
 import Matches from "../../_components/matches";
 import { api } from "~/trpc/server";
 import { getServerAuthSession } from "~/server/auth";
+import MatchWindow from "~/app/_components/matches/MatchWindow";
 
 export default async function Page() {
   const session = await getServerAuthSession();
   const matches = await api.user.getMatches(session?.user.id ?? "");
   return (
-    <div className="h-full w-fit">
+    <div className="flex h-full w-full flex-row">
       <Matches matches={matches} />
+      <MatchWindow />
     </div>
   );
 }
