4ab6b3e4947405bfadd5b37bdedec9fd1f02d10b
טל דבי <taldebi2@gmail.com>
2024-08-02 00:11:51 +0300

    added layout

--- src/app/_components/matches/MatchWindow.tsx
+++ src/app/_components/matches/MatchWindow.tsx
@@ -1,44 +1,67 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React from "react";
 import { ScrollArea, ScrollBar } from "../shadcn/scroll-area";
 import Image from "next/image";
-import exampleImage from "public/assets/loading-image.png";
+import exampleImage from "public/assets/profilePic.png";
 import { Separator } from "../shadcn/separator";
+import { AspectRatio } from "../shadcn/aspect-ratio";
+import { MapPin } from "lucide-react";
 
 const matchData = [
   {
     artist: "Ornella Binni",
+    age: 23,
+    city: "Holon",
     art: exampleImage,
   },
   {
     artist: "Tom Byrom",
+    age: 20,
+    city: "Tel-Aviv",
     art: exampleImage,
   },
   {
     artist: "Vladimir Malyavko",
+    age: 25,
+    city: "Eilat",
     art: exampleImage,
   },
 ];
 
 const MatchWindow: React.FC = () => {
   return (
-    <div className="m-24 mr-2 h-full w-8/12 flex-row rounded-md border border-primary">
-      <div>
-        <ScrollArea className="w-6/12 flex-col whitespace-nowrap align-middle">
-          {matchData.map((data, index) => (
+    <div className="h-8/12 m-24 mr-2 flex w-8/12 rounded-md border-4 border-primary">
+      <ScrollArea className="w-6/12">
+        {matchData.map((data, index) => (
+          <AspectRatio key={index}>
             <Image
-              key={index}
               src={data.art}
               alt={`Photo by ${data.artist}`}
-              className="w-full overflow-hidden rounded-md object-contain"
-              width={300}
-              height={400}
+              className="object-cover"
+              fill
             />
-          ))}
-          <ScrollBar orientation="vertical" />
-        </ScrollArea>
-      </div>
-      <Separator orientation="vertical" className="text-white-500 ml-1 mr-1" />
+          </AspectRatio>
+        ))}
+        <ScrollBar orientation="vertical" />
+      </ScrollArea>
+      <Separator
+        orientation="vertical"
+        className="ml-0.5 mr-0.5 w-0.5 bg-secondary"
+      />
+      <ScrollArea className="w-6/12 bg-primary pl-4">
+        {matchData.map((data, index) => (
+          <AspectRatio key={index}>
+            <div className="flex h-full w-full flex-col justify-center gap-1 object-cover font-bold">
+              <div className="text-4xl">{`${data.artist}, ${data.age}`}</div>
+              <div className="flex items-end text-2xl">
+                <MapPin className="h-8 w-8" />
+                {data.city}
+              </div>
+            </div>
+          </AspectRatio>
+        ))}
+        <ScrollBar orientation="vertical" />
+      </ScrollArea>
     </div>
   );
 };
