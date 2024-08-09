8a9909f384f35503b323ebe8093461db766b0679
טל דבי <taldebi2@gmail.com>
2024-08-09 15:19:50 +0300

    added buttons

--- src/app/_components/matches/MatchWindow.tsx
+++ src/app/_components/matches/MatchWindow.tsx
@@ -1,67 +1,83 @@
 "use client";
 import React from "react";
-import { ScrollArea, ScrollBar } from "../shadcn/scroll-area";
 import Image from "next/image";
 import exampleImage from "public/assets/profilePic.png";
 import { Separator } from "../shadcn/separator";
 import { AspectRatio } from "../shadcn/aspect-ratio";
-import { MapPin } from "lucide-react";
+import { CheckIcon, MapPin } from "lucide-react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../shadcn/carousel";
+import { Button } from "../shadcn/button";
+import { XIcon } from "lucide-react";
 
-const matchData = [
-  {
-    artist: "Ornella Binni",
-    age: 23,
-    city: "Holon",
-    art: exampleImage,
-  },
-  {
-    artist: "Tom Byrom",
-    age: 20,
-    city: "Tel-Aviv",
-    art: exampleImage,
-  },
-  {
-    artist: "Vladimir Malyavko",
-    age: 25,
-    city: "Eilat",
-    art: exampleImage,
-  },
-];
+const currentMatch = {
+  artist: "Ornella Binni",
+  age: 23,
+  city: "Holon",
+  pictures: [exampleImage, exampleImage, exampleImage],
+};
 
 const MatchWindow: React.FC = () => {
   return (
-    <div className="h-8/12 m-24 mr-2 flex w-8/12 rounded-md border-4 border-primary">
-      <ScrollArea className="w-6/12">
-        {matchData.map((data, index) => (
-          <AspectRatio key={index}>
-            <Image
-              src={data.art}
-              alt={`Photo by ${data.artist}`}
-              className="object-cover"
-              fill
-            />
-          </AspectRatio>
-        ))}
-        <ScrollBar orientation="vertical" />
-      </ScrollArea>
-      <Separator
-        orientation="vertical"
-        className="ml-0.5 mr-0.5 w-0.5 bg-secondary"
-      />
-      <ScrollArea className="w-6/12 bg-primary pl-4">
-        {matchData.map((data, index) => (
-          <AspectRatio key={index}>
+    <div className="h-8/12 relative m-24 mr-2 flex w-8/12">
+      <div className="flex h-full w-full rounded-md border-4 border-primary">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          orientation="vertical"
+          className="w-6/12"
+        >
+          <CarouselContent className="h-[73vh]">
+            {currentMatch.pictures.map((picture, index) => (
+              <CarouselItem key={index}>
+                <AspectRatio>
+                  <Image
+                    src={picture}
+                    alt={`Photo by ${currentMatch.artist}`}
+                    objectFit="cover"
+                    fill
+                  />
+                </AspectRatio>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <Separator
+          orientation="vertical"
+          className="w-0.25 ml-0.5 mr-0.5 bg-secondary"
+        />
+        <div className="w-6/12 bg-primary pl-4">
+          <AspectRatio>
             <div className="flex h-full w-full flex-col justify-center gap-1 object-cover font-bold">
-              <div className="text-4xl">{`${data.artist}, ${data.age}`}</div>
+              <div className="text-4xl">{`${currentMatch.artist}, ${currentMatch.age}`}</div>
               <div className="flex items-end text-2xl">
                 <MapPin className="h-8 w-8" />
-                {data.city}
+                {currentMatch.city}
               </div>
             </div>
           </AspectRatio>
-        ))}
-        <ScrollBar orientation="vertical" />
-      </ScrollArea>
+        </div>
+      </div>
+      <div className="absolute -bottom-8 left-1/2 flex -translate-x-1/2 transform gap-8">
+        <Button
+          className="h-16 w-16 rounded-full border border-primary bg-white text-red-700 hover:bg-red-400"
+          onClick={() => console.log("pass")}
+        >
+          <XIcon />
+        </Button>
+        <Button
+          className="h-16 w-16 rounded-full border border-primary bg-white text-green-700 hover:bg-green-400"
+          onClick={() => console.log("match")}
+        >
+          <CheckIcon />
+        </Button>
+      </div>
     </div>
   );
 };
