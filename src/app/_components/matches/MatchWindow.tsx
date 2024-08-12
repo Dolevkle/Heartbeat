fc34cd06a6ad2192a46100c24083a764065ad0a4
טל דבי <taldebi2@gmail.com>
2024-08-12 15:23:04 +0300

    added dynamic progress bar

--- src/app/_components/matches/MatchWindow.tsx
+++ src/app/_components/matches/MatchWindow.tsx
@@ -1,20 +1,18 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import exampleImage from "public/assets/profilePic.png";
-import { Separator } from "../shadcn/separator";
 import { AspectRatio } from "../shadcn/aspect-ratio";
 import { CheckIcon, MapPin } from "lucide-react";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "../shadcn/carousel";
+import { Carousel, CarouselContent, CarouselItem } from "../shadcn/carousel";
 import { Button } from "../shadcn/button";
 import { XIcon } from "lucide-react";
 import { Progress } from "../shadcn/progress";
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "../shadcn/resizable";
 
 const currentMatch = {
   artist: "Ornella Binni",
@@ -24,69 +22,87 @@ const currentMatch = {
 };
 
 const MatchWindow: React.FC = () => {
-  const [progress, setProgress] = useState<number>(13);
+  const [progress, setProgress] = useState<number>(1);
 
-  useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500);
-    return () => clearTimeout(timer);
-  }, []);
+  const handleNext = () => {
+    setProgress((prev) => prev + 1);
+  };
+
+  const handlePrevious = () => {
+    setProgress((prev) => prev - 1);
+  };
 
   return (
-    <div className="h-8/12 relative m-24 mr-2 flex w-8/12">
+    <div className="h-8/12 relative m-24 flex w-8/12">
       <div className="flex h-full w-full rounded-md border-4 border-primary">
-        <Carousel
-          opts={{
-            align: "start",
-          }}
-          orientation="vertical"
-          className="w-6/12"
-        >
-          <CarouselContent className="h-[73vh]">
-            {currentMatch.pictures.map((picture, index) => (
-              <CarouselItem key={index}>
-                <AspectRatio>
-                  <Image
-                    src={picture}
-                    alt={`Photo by ${currentMatch.artist}`}
-                    objectFit="cover"
-                    fill
-                  />
-                </AspectRatio>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
-        <Separator
-          orientation="vertical"
-          className="w-0.25 ml-0.5 mr-0.5 bg-secondary"
-        />
-        <div className="w-6/12 bg-primary pl-4">
-          <AspectRatio>
-            <div className="flex h-full w-full flex-col justify-center gap-1 object-cover font-bold">
-              <div className="text-4xl">{`${currentMatch.artist}, ${currentMatch.age}`}</div>
-              <div className="flex items-end text-2xl">
-                <MapPin className="h-8 w-8" />
-                {currentMatch.city}
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel
+            className="h-full w-6/12"
+            defaultSize={52}
+            minSize={30}
+          >
+            <Carousel
+              opts={{
+                align: "start",
+              }}
+              orientation="vertical"
+              handlePrevious={handlePrevious}
+              handleNext={handleNext}
+            >
+              <CarouselContent className="h-[73vh]">
+                {currentMatch.pictures.map((picture, index) => (
+                  <CarouselItem key={index}>
+                    <AspectRatio>
+                      <Image
+                        src={picture}
+                        alt={`Photo by ${currentMatch.artist}`}
+                        objectFit="cover"
+                        fill
+                      />
+                    </AspectRatio>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+          </ResizablePanel>
+          <ResizableHandle className="w-0.5 bg-black text-white" withHandle />
+          <ResizablePanel
+            className="w-6/12 bg-primary pl-4"
+            defaultSize={50}
+            minSize={30}
+          >
+            <AspectRatio>
+              <div className="flex h-full w-full flex-col justify-center gap-1 object-cover font-bold">
+                <div className="text-4xl">{`${currentMatch.artist}, ${currentMatch.age}`}</div>
+                <div className="flex items-end text-2xl">
+                  <MapPin className="h-8 w-8" />
+                  {currentMatch.city}
+                </div>
               </div>
-            </div>
-          </AspectRatio>
-        </div>
+            </AspectRatio>
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
       <Progress
-        value={progress}
+        value={(progress / currentMatch.pictures.length) * 100}
+        max={100}
         orientation="vertical"
-        className="absolute w-[60%]"
+        className="absolute -left-8 h-[25%] w-2"
       />
       <div className="absolute -bottom-8 left-1/2 flex -translate-x-1/2 transform gap-8">
         <Button
           className="h-16 w-16 rounded-full border border-primary bg-white text-red-700 hover:bg-red-400"
-          onClick={() => console.log("pass")}
+          onClick={() => {
+            console.log("pass");
+          }}
         >
           <XIcon />
         </Button>
         <Button
           className="h-16 w-16 rounded-full border border-primary bg-white text-green-700 hover:bg-green-400"
-          onClick={() => console.log("match")}
+          onClick={() => {
+            console.log("match");
+          }}
         >
           <CheckIcon />
         </Button>
