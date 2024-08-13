eddf3e85753a168883271684e24e87dbef132afb
טל דבי <taldebi2@gmail.com>
2024-08-13 11:11:07 +0300

    matches side bar

--- src/app/_components/UserCard.tsx
+++ src/app/_components/UserCard.tsx
@@ -11,27 +11,57 @@ export default function UserCard({ user, onClick, isSelected }: Props) {
   const divRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div
-      className={`flex rounded-lg border-2 ${isSelected ? "border-white" : "border-transparent"} p-1`}
-      ref={divRef}
-      onClick={() => (onClick && user ? onClick(user) : null)}
-    >
-      <Avatar className="hidden h-9 w-9 sm:flex">
-        <AvatarImage src={user?.image ?? ""} alt="Avatar" />
-        <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
-      </Avatar>
-      <Avatar className="hidden h-9 w-9 sm:flex">
-        <AvatarImage src={user?.image ?? ""} alt="Avatar" />
-        <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
-      </Avatar>
-      <Avatar className="hidden h-9 w-9 sm:flex">
-        <AvatarImage src={user?.image ?? ""} alt="Avatar" />
-        <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
-      </Avatar>
-      <Avatar className="hidden h-9 w-9 sm:flex">
-        <AvatarImage src={user?.image ?? ""} alt="Avatar" />
-        <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
-      </Avatar>
+    <div className="flex">
+      <div
+        className={`flex rounded-lg border-2 ${isSelected ? "border-white" : "border-transparent"} p-1`}
+        ref={divRef}
+        onClick={() => (onClick && user ? onClick(user) : null)}
+      >
+        <Avatar className="hidden h-9 w-9 sm:flex">
+          <AvatarImage src={user?.image ?? ""} alt="Avatar" />
+          <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
+        </Avatar>
+      </div>
+      <div
+        className={`flex rounded-lg border-2 ${isSelected ? "border-white" : "border-transparent"} p-1`}
+        ref={divRef}
+        onClick={() => (onClick && user ? onClick(user) : null)}
+      >
+        <Avatar className="hidden h-9 w-9 sm:flex">
+          <AvatarImage src={user?.image ?? ""} alt="Avatar" />
+          <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
+        </Avatar>
+      </div>
+      <div
+        className={`flex rounded-lg border-2 ${isSelected ? "border-white" : "border-transparent"} p-1`}
+        ref={divRef}
+        onClick={() => (onClick && user ? onClick(user) : null)}
+      >
+        <Avatar className="hidden h-9 w-9 sm:flex">
+          <AvatarImage src={user?.image ?? ""} alt="Avatar" />
+          <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
+        </Avatar>
+      </div>
+      <div
+        className={`flex rounded-lg border-2 ${isSelected ? "border-white" : "border-transparent"} p-1`}
+        ref={divRef}
+        onClick={() => (onClick && user ? onClick(user) : null)}
+      >
+        <Avatar className="hidden h-9 w-9 sm:flex">
+          <AvatarImage src={user?.image ?? ""} alt="Avatar" />
+          <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
+        </Avatar>
+      </div>
+      <div
+        className={`flex rounded-lg border-2 ${isSelected ? "border-white" : "border-transparent"} p-1`}
+        ref={divRef}
+        onClick={() => (onClick && user ? onClick(user) : null)}
+      >
+        <Avatar className="hidden h-9 w-9 sm:flex">
+          <AvatarImage src={user?.image ?? ""} alt="Avatar" />
+          <AvatarFallback>{user?.name?.charAt(0) ?? ""}</AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   );
 }
