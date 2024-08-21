49c9c7c311448d83dbf7c18887df1f992c2cbd41
טל דבי <taldebi2@gmail.com>
2024-08-22 00:49:23 +0300

    added filtered get metches but refresh doesnt work still

--- src/app/home/matches/page.tsx
+++ src/app/home/matches/page.tsx
@@ -1,43 +1,29 @@
-import Matches from "../../_components/matches";
-import { api as serverApi } from "~/trpc/server";
-import { getServerAuthSession } from "~/server/auth";
-import type { User, Match } from "@prisma/client";
-import type { ParticipantDict } from "~/server/api/routers/match/match";
 import MatchWindowClient from "~/app/_components/matches/MatchWindowClient";
+import { getServerAuthSession } from "~/server/auth";
+import { api as serverApi } from "~/trpc/server";
 
 export default async function Page() {
   const session = await getServerAuthSession();
-  const potentialMatches: Match[] = await serverApi.match.getMatches(
-    session?.user.id ?? "",
-  );
+  const userId = session?.user.id ?? "";
+
+  // Fetch data server-side
+  const potentialMatches = await serverApi.match.getMatches(userId);
 
   const ids = potentialMatches?.flatMap(({ userStatuses }) => {
     if (userStatuses) {
-      const participantIds = Object.keys(userStatuses as ParticipantDict);
-      return participantIds.filter((id) => id !== session?.user?.id);
+      const participantIds = Object.keys(userStatuses);
+      return participantIds.filter((id) => id !== userId);
     }
-
     return [];
   });
 
   const users = ids.length > 0 ? await serverApi.user.findUsersByIds(ids) : [];
 
-  const orderedUsers: User[] = ids?.map(
-    (id) => users.find((user) => user.id === id) as User,
-  );
-
   return (
-    <div className="flex h-full w-full flex-row">
-      <Matches potentialMatches={orderedUsers} isLoadingUsers={false} />
-      {orderedUsers.length > 0 ? (
-        <MatchWindowClient
-          potentialMatch={orderedUsers[0]}
-          matchId={potentialMatches[0]?.id ?? ""}
-          sessionUserId={session?.user.id ?? ""}
-        />
-      ) : (
-        "No matches"
-      )}
-    </div>
+    <MatchWindowClient
+      userId={userId}
+      initialPotentialMatches={potentialMatches}
+      initialUsers={users}
+    />
   );
 }
