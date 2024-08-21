9c76de271ac02d5a6c5a63375fb67c362a47173f
tkgs16 <160175519+tkgs16@users.noreply.github.com>
2024-08-22 00:57:30 +0300

    images saves now in users db, need to fix css and show the images on first component

--- src/app/_components/image-uploader.tsx
+++ src/app/_components/image-uploader.tsx
@@ -1,12 +1,27 @@
 'user client'
 
-import React from "react";
-import { UploadButton } from "~/utils/uploadthing";
+import Image from "next/image";
+import { UploadDropzone } from "@uploadthing/react";
+import React, { useState } from "react";
 const ImageUpload = () => {
+    const [imageUrl, setImageUrl] = useState<string>('');
 
     return (
     <div>
-        <UploadButton endpoint='imageUploader'/>
+        <UploadDropzone endpoint='imageUploader'
+                onClientUploadComplete={(res) => {
+                    setImageUrl(res[0].url)
+                  }}
+                  onUploadError={(error: Error) => {
+                    alert(`ERROR! ${error.message}`);
+                  }}/>
+
+        {imageUrl.length ? (
+            <div>
+            <Image src={imageUrl} alt= 'my image' width={500} height={300}/>
+            </div>
+        
+        ):null}
     </div>
     )
 }
