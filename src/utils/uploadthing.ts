901ab419fedc2fdb97fd8968888e0b7ff1fa2033
tkgs16 <160175519+tkgs16@users.noreply.github.com>
2024-08-21 20:06:57 +0300

    upload working, also showing the image but these arent connected

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -5,6 +5,15 @@ import {
    
   import type { OurFileRouter } from "~/app/api/uploadthing/core";
    
+  export const UploadButton = generateUploadButton<OurFileRouter>();
+  export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+import {
+    generateUploadButton,
+    generateUploadDropzone,
+  } from "@uploadthing/react";
+   
+  import type { OurFileRouter } from "~/app/api/uploadthing/core";
+   
   
   export const UploadButton = generateUploadButton<OurFileRouter>();
   export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
