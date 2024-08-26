e48689f7ca154c9a33d21d3a0b717ab188272a19
Nir <nirkopp@gmail.com>
2024-08-26 20:51:42 +0300

    CR fixes: removed unneeded useEffect and cleened a function

--- src/app/_components/chats/ChatInput.tsx
+++ src/app/_components/chats/ChatInput.tsx
@@ -16,7 +16,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { api } from "~/trpc/react";
 import { useSession } from "next-auth/react";
-import { useEffect, useRef } from "react";
+import {  useRef } from "react";
 import ImageUploadDialog from "./ImageUploadDialog";
 
 export const messageFormSchema = z
@@ -84,7 +84,8 @@ export default function ChatInput({ chatId }: Props) {
     formRef.current?.requestSubmit();
   };
 
-  form.register("imageUrl", { required: false });
+    form.register("imageUrl", { required: false });
+
 
   return (
     <Form {...form}>
