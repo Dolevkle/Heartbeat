90fc4546d9910130a5c20a26d41c6eff2272709b
טל דבי <taldebi2@gmail.com>
2024-08-30 17:47:25 +0300

    kaki

---
+++ src/app/_components/profile/ProfileClient.tsx
@@ -0,0 +1,43 @@
+"use client";
+import PersonalityRadarChart from "~/app/_components/profile/PersonalityRadarChart";
+import TraitCarousel, {
+  type CarouselHandle,
+} from "~/app/_components/profile/TraitCarousel";
+import { useRef } from "react";
+import { ScrollArea } from "@components/scroll-area";
+import type { User } from "@prisma/client";
+import type { Personality } from "~/app/signup/types";
+import UserDetailsDisplay from "./UserDetails/UserDetailsDisplay";
+import ChosenPlaylistDisplay from "./ChosenPlaylist/ChosenPlaylistDisplay";
+
+interface ProfileClientProps {
+  user: User;
+}
+
+export default function ProfileClient({ user }: ProfileClientProps) {
+  const carouselRef = useRef<CarouselHandle>(null);
+
+  return (
+    <ScrollArea className="rounded-md">
+      <div className="grid max-h-screen grid-cols-4 gap-y-5 p-6">
+        <div className="col-span-2 h-fit justify-start ">
+          <UserDetailsDisplay />
+        </div>
+        <div className="col-span-2 row-start-2 w-full	justify-self-center">
+          <ChosenPlaylistDisplay />
+        </div>
+        <div className="col-span-2  h-full">
+          {user?.personality && (
+            <PersonalityRadarChart
+              personality={user?.personality as Personality}
+              carouselRef={carouselRef}
+            />
+          )}
+        </div>
+        <div className="col-span-2 col-start-3 flex h-fit w-full  items-center justify-center">
+          <TraitCarousel ref={carouselRef} />
+        </div>
+      </div>
+    </ScrollArea>
+  );
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,39 +1,9 @@
-import PersonalityRadarChart from "~/app/_components/profile/PersonalityRadarChart";
-import { useSession } from "next-auth/react";
 import { getServerAuthSession } from "~/server/auth";
-import TraitCarousel, {
-  CarouselHandle,
-} from "~/app/_components/profile/TraitCarousel";
-import ChosenPlaylistDisplay from "../_components/profile/ChosenPlaylist/ChosenPlaylistDisplay";
-import UserDetailsDisplay from "../_components/profile/UserDetails/UserDetailsDisplay";
-import {useRef } from "react";
-import { ScrollArea } from "@components/scroll-area";
+import ProfileClient from "../_components/profile/ProfileClient";
 
 export default async function Page() {
   const session = await getServerAuthSession();
-  const carouselRef = useRef<CarouselHandle>(null);
+  const user = session?.user;
 
-  return (
-    <ScrollArea className="rounded-md">
-      <div className="grid max-h-screen grid-cols-4 gap-y-5 p-6">
-        <div className="col-span-2 h-fit justify-start ">
-          <UserDetailsDisplay />
-        </div>
-        <div className="col-span-2 row-start-2 w-full	justify-self-center">
-          <ChosenPlaylistDisplay />
-        </div>
-        <div className="col-span-2  h-full">
-          {session?.user.personality && (
-            <PersonalityRadarChart
-              personality={session.user.personality}
-              carouselRef={carouselRef}
-            />
-          )}
-        </div>
-        <div className="col-span-2 col-start-3 flex h-fit w-full  items-center justify-center">
-          <TraitCarousel ref={carouselRef} />
-        </div>
-      </div>
-    </ScrollArea>
-  );
+  return <ProfileClient user={user} />;
 }
