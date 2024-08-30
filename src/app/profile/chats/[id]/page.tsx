436e298082353ddf0a5c0ed48fb27469b5cceb14
Dolevkle <dolevkle@gmail.com>
2024-08-30 15:32:46 +0300

    [FIXED] - chat info card. it is now a drawer

--- src/app/profile/chats/[id]/page.tsx
+++ src/app/profile/chats/[id]/page.tsx
@@ -5,7 +5,6 @@ import { useSearchParams } from "next/navigation";
 import { api } from "~/trpc/react";
 import { type User } from "@prisma/client";
 import ChatMessages from "~/app/_components/chats/ChatMessages";
-import ChatInfoCard from "~/app/_components/chats/ChatInfoCard";
 
 export default function Page({ params }: { params: { id: string } }) {
   const searchParams = useSearchParams();
@@ -17,11 +16,11 @@ export default function Page({ params }: { params: { id: string } }) {
   });
 
   return (
-    <div className="grid grid-cols-3 grid-rows-12 h-full w-full">
+    <div className="grid h-full w-full grid-rows-12">
       <ChatHeader user={parsedUser} />
       <ChatMessages messages={messages} chatId={params.id} />
       <ChatInput chatId={params.id} />
-      <ChatInfoCard user={parsedUser}/>
+      {/*<ChatInfoCard user={parsedUser}/>*/}
     </div>
   );
 }
